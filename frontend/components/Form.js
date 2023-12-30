4f724a493c8fab18f36f1bb5a46c5f6f0a9565ae
Cory Evan Johnson <cejohns3@gmail.com>
2023-12-30 15:40:42 -0500

    Almost There 2

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import { useState } from 'react';
 
 const validationErrors = {
   fullNameTooShort: 'full name must be at least 3 characters',
@@ -23,54 +24,79 @@ const toppings = [
 ];
 
 export default function Form() {
-  const { register, handleSubmit, formState: { errors, isSubmitSuccessful } } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitSuccessful },
+    reset,
+  } = useForm({
     resolver: yupResolver(validationSchema),
   });
+
+  const [successMessage, setSuccessMessage] = useState(null);
 
   const onSubmit = (data) => {
     console.log('Submitted data:', data);
     // Perform any necessary actions with the form data
+    const { fullName, size, toppings } = data;
+
+    let message = `Thank you for your order, ${fullName}! Your ${size} pizza`;
+  
+    const selectedToppings = Object.entries(toppings)
+      .filter(([_, isSelected]) => isSelected)
+      .map(([topping]) => topping);
+  
+    if (selectedToppings.length === 0) {
+      message += ' with no toppings';
+    } else {
+      message += ` with ${selectedToppings.join(', ')} toppings`;
+    }
+  
+    message += ' is on the way.';
+  
+    setSuccessMessage(message);
+    reset(); // Reset the form after successful submission
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <h2>Order Your Pizza</h2>
-      {isSubmitSuccessful && <div className='success'>Thank you for your order!</div>}
-      {errors.fullName && <div className='error'>{errors.fullName.message}</div>}
-      {errors.size && <div className='error'>{errors.size.message}</div>}
+    <h2>Order Your Pizza</h2>
+    {isSubmitSuccessful && <div className='success'>{successMessage}</div>}
+    {errors.fullName && <div className='error'>{errors.fullName.message}</div>}
+    {errors.size && <div className='error'>{errors.size.message}</div>}
 
-      <div className="input-group">
-        <div>
-          <label htmlFor="fullName">Full Name</label><br />
-          <input placeholder="Type full name" id="fullName" type="text" {...register('fullName')} />
-        </div>
+    <div className="input-group">
+      <div>
+        <label htmlFor="fullName">Full Name</label><br />
+        <input placeholder="Type full name" id="fullName" type="text" {...register('fullName')} />
       </div>
+    </div>
 
-      <div className="input-group">
-        <div>
-          <label htmlFor="size">Size</label><br />
-          <select id="size" {...register('size')}>
-            <option value="">----Choose Size----</option>
-            <option value="S">Small</option>
-            <option value="M">Medium</option>
-            <option value="L">Large</option>
-          </select>
-        </div>
+    <div className="input-group">
+      <div>
+        <label htmlFor="size">Size</label><br />
+        <select id="size" {...register('size')}>
+          <option value="">----Choose Size----</option>
+          <option value="S">Small</option>
+          <option value="M">Medium</option>
+          <option value="L">Large</option>
+        </select>
       </div>
+    </div>
 
-      <div className="input-group">
-        {toppings.map((topping) => (
-          <label key={topping.topping_id}>
-            <input
-              type="checkbox"
-              {...register(`toppings.${topping.text}`)}
-            />
-            {topping.text}<br />
-          </label>
-        ))}
-      </div>
+    <div className="input-group">
+      {toppings.map((topping) => (
+        <label key={topping.topping_id}>
+          <input
+            type="checkbox"
+            {...register(`toppings.${topping.text}`)}
+          />
+          {topping.text}<br />
+        </label>
+      ))}
+    </div>
 
-      <input type="submit" disabled={Object.keys(errors).length > 0} />
-    </form>
+    <input type="submit" disabled={Object.keys(errors).length > 0} />
+  </form>
   );
 }
